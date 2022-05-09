08aed375c75e2d098beb86f32d627c6e3afd7829
ing-arriola <ingeniero.arriola@gmail.com>
2022-05-09 17:31:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    search:"",
+    images:[], 
+    loading:true
+  }
+
+
+
+  handleSearch = async () => {
+    //axios get from giphy
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${this.state.search}&limit=10`)
+    console.log(res.data.data)
+    this.setState({images:res.data.data, loading:false})
+  }
+
+  render() {
+    return (
+      <>
+        <input  placeholder='Que buscas' value={this.state.search}  onChange={ (e) => this.setState({search:e.target.value})} />
+        <button onClick={this.handleSearch}>Buscar</button>
+        {this.state.loading && this.state.images.length === 0 ? null : <h1>Cargando...</h1> }
+        {!this.state.loading &&  <div>{JSON.stringify(this.state.images)}</div> }
+      </>
+    )
+  }
 }
-
-export default App;
